4b1e6f7ddf9d1f6d8bf8ef8c342fb60339495278
David Bibby <davidsbibby@gmail.com>
2022-12-16 14:14:18 +0000

    Added HomeAssistant MQTT discovery

--- heatmiser-mqtt.py
+++ heatmiser-mqtt.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3.9
 
 import asyncio
+import random
+import re
+import sys
+import json
+from itertools import chain
 from configparser import ConfigParser
+from datetime import datetime
+from paho.mqtt import client as mqtt_client
 from heatmiser.network import HeatmiserNetwork
 from heatmiser.device import HeatmiserDevice
 from heatmiser.logging import log
-from itertools import chain
-from paho.mqtt import client as mqtt_client
-import random
 
 
-hm_task = None
-config = None
-mqtt = None
-
+HM_TASK = None
+CONFIG = None
+MQTT = None
+BASE_TOPIC = None
+hm_entity_id = None
 
 """ Generate a range from a hyphonated,comma seperated
 string of numbers: e.g. 1,2,5-7,9
@@ -25,11 +30,43 @@ def rangeString(commaString):
     return chain(*[hyphenRange(r) for r in commaString.split(',')])
 
 
+def hm_advertise_device(device):
+    log('debug', f'Advertising device with ID {device.id}')
+    payload = {
+        "~": f"{BASE_TOPIC}/{device.id}",
+        "name": f"Heatmiser {device.TYPE_STR} {device.id}",
+        "unique_id": f"{hm_entity_id}-{device.id}",
+        "modes": ["off", "heat", "cool"],
+        "min_temp": 5,
+        "max_temp": 35,
+        "curr_temp_t": "~/room_temp/state",
+        "mode_stat_t": "~/mode/state",
+        "temperature_state_topic": "~/set_temp/state",
+        "action_topic": "~/heating_state/state"
+    }
+    log('debug', f"{BASE_TOPIC}/{device.id}/config -> {json.dumps(payload)}")
+    MQTT.publish(f"{BASE_TOPIC}/{device.id}/config", json.dumps(payload))
+    MQTT.subscribe(f"{BASE_TOPIC}/{device.id}/+/set")
+
+
 def hm_device_updated(device, param_name, value):
-    global mqtt
+    global MQTT
     log('info', f"HM Device Updated - ID: {device.id}, {param_name} = {value}")
-    if mqtt is not None:
-        mqtt.publish(f"{config['MQTT']['pub_topic']}/{device.id}/{param_name}", str(value))
+    if MQTT is not None:
+        if param_name == "enabled":
+            param_name = "mode"
+            if value:
+                value = "cool" if device.frost_mode else "heat"
+            else:
+                value = "off"
+        elif param_name == "frost_mode":
+            param_name = "mode"
+            value = "cool" if value else "heat"
+        elif param_name == "heating_state":
+            value = "heating" if value else "idle"
+        elif param_name == "datetime":
+            value = value.timestamp()
+        MQTT.publish(f"{BASE_TOPIC}/{device.id}/{param_name}/state", value)
 
 
 def connect_mqtt(client_id, broker, port, username, password):
@@ -49,43 +86,49 @@ def connect_mqtt(client_id, broker, port, username, password):
 def handle_mqtt_message(client, userdata, msg):
     log('info', f'Received `{msg.payload.decode()}` from `{msg.topic}` topic')
 
- 
+
 async def mqtt_monitor():
+    global MQTT
+
     log('info', 'MQTT Started')
-#    await asyncio.sleep(15)
-    global mqtt
+
     while True:
-        mqtt.loop()
+        MQTT.loop()
         await asyncio.sleep(1)
-        log('debug', f'MQTT State: {mqtt._state}')
-        if not mqtt.is_connected():
+        if not MQTT.is_connected():
             break
+
     log('info', 'MQTT End')
 
 
 async def main():
-    global mqtt
+    global MQTT, BASE_TOPIC, hm_entity_id
+
     HeatmiserDevice.on_param_change = hm_device_updated
+
     try:
-        hm_config = config['Heatmiser']
+        hm_config = CONFIG['Heatmiser']
         serial_port = hm_config['serial_port']
         device_ids = hm_config['device_ids']
-        mqtt_config = config['MQTT']
+        mqtt_config = CONFIG['MQTT']
         broker = mqtt_config['broker']
         port = int(mqtt_config['port'])
         username = mqtt_config['username']
         password = mqtt_config['password']
     except KeyError as e:
         print(e)
-        exit()
+        sys.exit()
+
     client_id = f'heatmiser-mqtt-{random.randint(0, 1000)}'
+    hm_entity_id = re.sub("[^a-zA-Z0-9_-]", "", serial_port)
+    BASE_TOPIC = f"{mqtt_config['topic']}/climate/{hm_entity_id}"
 
     while True:
         # Open the serial connection to the Heatmiser network and start up the monitors
         hmn = HeatmiserNetwork(serial_port, rangeString(device_ids))
-        mqtt = connect_mqtt(client_id, broker, port, username, password)
-        mqtt.on_message = handle_mqtt_message
-        mqtt.subscribe(mqtt_config['sub_topic'])
+        hmn.on_device_discovered = hm_advertise_device
+        MQTT = connect_mqtt(client_id, broker, port, username, password)
+        MQTT.on_message = handle_mqtt_message
         tasks = set()
         tasks.add(asyncio.create_task(hmn.run()))
         tasks.add(asyncio.create_task(mqtt_monitor()))
@@ -99,7 +142,7 @@ async def main():
 
         # At least one of the tasks has ended. Close the rest cleanly
         hmn.close()
-        mqtt.disconnect()
+        MQTT.disconnect()
         for task in tasks:
             if task.done():
                 continue
@@ -108,15 +151,15 @@ async def main():
                 await task
             except asyncio.CancelledError:
                 pass
-        
+
         # Pause then attempt to restart from the top
         log('info', 'Tasks ended... reconnecting in 10...')
         await asyncio.sleep(10)
 
 
 if __name__ == "__main__":
-    config = ConfigParser()
-    config.read('config.ini')
+    CONFIG = ConfigParser()
+    CONFIG.read('config.ini')
 
     asyncio.run(main())
     print("Done")
